ce5826849388a21b8240f10044b48e9dddbeb988
Overthrive <sara.leal.lessa@gmail.com>
2022-03-14 22:14:10 -0300

    Organizando o repositório

---
+++ backend/yastoa/src/main/java/br/com/yastoa/YastoaApplication.java
@@ -0,0 +1,13 @@
+package br.com.yastoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YastoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YastoaApplication.class, args);
+	}
+
+}
---
+++ backend/yastoa/src/test/java/br/com/yastoa/YastoaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yastoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YastoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
