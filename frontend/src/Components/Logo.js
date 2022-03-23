e36ed0665587b7b4ccdba24fe867b9af64da451e
Overthrive <sara.leal.lessa@gmail.com>
2022-03-23 10:30:39 -0300

    produtos

--- frontend/src/Components/Logo.js
+++ frontend/src/Components/Logo.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { Container } from "react-bootstrap";
 
 export default function Logo() {
   return (
     <div>
-      <img src="https://github.com/flpeandrade/YASTOA/blob/main/yastoaRecode/wwwroot/img/logo.png?raw=true" />
+      <img src="https://github.com/flpeandrade/YASTOA/blob/main/yastoaRecode/wwwroot/img/logo.png?raw=true" alt="Logo" />
     </div>
   );
 }
